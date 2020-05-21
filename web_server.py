54bb1e5b690dedf43265f3f74b290cd3650f3941
Matthew Gorton <mgorton@mgorton.local>
2020-05-21 11:29:13 -0700

    corrected syntax for template - needs double {{}}

--- web_server.py
+++ web_server.py
@@ -23,11 +23,11 @@ def greet(name='Stranger'):
 
 @route('/test/<pagename>')            # matches /wiki/Learning_Python
 def show_wiki_page(pagename):
-    return template("Pagename is {pagename}", pagename=pagename)
+    return template("Pagename is {{pagename}}", pagename=pagename)
 
 @route('/<action>/<user>')            # matches /follow/defnull
 def user_api(action, user):
-    return template("Action: {action}, User: {user}", action=action, user=user)
+    return template("Action: {{action}}, User: {{user}}", action=action, user=user)
 
 
 def main():
