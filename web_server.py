46f4379864f0d487ab3173a5a112049fcadd2132
Matthew Gorton <mgorton@mgorton.local>
2020-05-21 10:14:43 -0700

    Initial commit - basic server with minimal routes

---
+++ web_server.py
@@ -0,0 +1,33 @@
+#!/usr/local/bin/python3
+import sys
+from bottle import route, run
+from bottle import template
+
+@route('/hello')
+def hello():
+    return "Hello World!"
+
+@route('/')
+@route('/hello/<name>')
+def greet(name='Stranger'):
+    return template('Hello {{name}}, how are you?', name=name)
+
+
+
+@route('/test/<pagename>')            # matches /wiki/Learning_Python
+def show_wiki_page(pagename):
+    print(f"Pagename is {pagename}"")
+
+@route('/<action>/<user>')            # matches /follow/defnull
+def user_api(action, user):
+    print(f"Action: {action}, User: {user}"")
+
+
+def main():
+    print("Running webserver")
+    run(host='localhost', port=8080, debug=True)
+
+
+if __name__ == '__main__':
+
+    main()
